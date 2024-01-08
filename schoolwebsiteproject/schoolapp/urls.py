e0b90cca28e425b015138623f6c78f64a37b23a5
Devikakpramod <93471403+Devikakpramod@users.noreply.github.com>
2024-01-08 10:57:22 +0530

    Update urls.py

--- schoolwebsiteproject/schoolapp/urls.py
+++ schoolwebsiteproject/schoolapp/urls.py
@@ -1,14 +1,13 @@
-from . import  views
 from django.urls import path
+from django.contrib import messages
+from . import views
 
 urlpatterns = [
-
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-    path('logout/',views.logout, name='logout'),
+    path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
-    path('form/',views.form,name='form'),
+    path('form/', views.form, name='form'),
     path('formfill/', views.formfill, name='formfill'),
-
-
+    path('data/', views.data, name='data'),
 ]
